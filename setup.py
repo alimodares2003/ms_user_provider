e27a105611c0faf9013f6f7809b46c4209d8a051
ali <s.a.modares.h@gmail.com>
2020-08-29 19:45:04 +0430

    first commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mesghal-user-provider",  # Replace with your own username
+    version="0.0.1",
+    author="Ali Modares",
+    author_email="s.a.modares.h@gmail.com",
+    description="mesghal user model",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alimodares2003/ms_user_provider.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
